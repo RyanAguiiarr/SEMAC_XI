b511800e8ddb10fdf81a8b3b84d8aec300a0b584
ryan <ryan.c.aguiiarr@gmail.com>
2025-10-18 15:57:26 -0300

    deploy 10

--- site_semac-20250919T183043Z-1-001/site_semac/SEMAC/src/components/Programacao.tsx
+++ site_semac-20250919T183043Z-1-001/site_semac/SEMAC/src/components/Programacao.tsx
@@ -201,7 +201,7 @@ const InscricaoModal: React.FC<InscricaoModalProps> = ({
 
     try {
       await axios.post(
-        `https://efda425b5234.ngrok-free.app/inscricao?palestra_id=${evento.id}`,
+        `https://8b9f241d06c1.ngrok-free.app/inscricao?palestra_id=${evento.id}`,
         {
           nome_completo: formData.nome_completo,
           email: formData.email,
